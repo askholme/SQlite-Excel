a75df72906143f435af9e0eb306c912e9eafae3c
Ask Holme <holme.ask@bcg.com>
2012-05-20 14:06:27 +0200

    VS project

---
+++ C-sharp/ExcelSQlite/ExcelSQlite/AddIn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExcelDna.Integration;
+
+namespace ExcelSQlite
+{
+    public class AddIn
+    {
+         [ExcelFunction(Description="My first Excel-DNA function")]
+        public static string MyFirstFunction(string name)
+        {
+            return "Hello " + name;
+        }
+
+
+
+    }   
+}
